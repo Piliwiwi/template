417c14b0e851fa690736d2a5bbe9808a2ef52eb4
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-11 19:08:33 -0400

    feat(app): initial and wildcard routes in routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'visitor/home', pathMatch: 'full'
+  },
+  { 
+    path: 'visitor', 
+    component: AppComponent,
+    loadChildren: () => import('./Visitor/visitor/visitor.module')
+      .then(module => module.VisitorModule)
+  },
+  {
+    path: '**', redirectTo: '/home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
