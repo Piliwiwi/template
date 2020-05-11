cda42524d4a74204725475bf5fa8e642db112171
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-11 19:10:33 -0400

    feat(visitor): home screen path in visitor children

--- src/app/Visitor/visitor/visitor-routing.module.ts
+++ src/app/Visitor/visitor/visitor-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VisitorComponent } from './visitor.component';
+import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: VisitorComponent,
+    children: [
+      { path: 'home', component: HomeScreenComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
