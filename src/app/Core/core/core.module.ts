7c2fea1b938c7b042749ba063e78ec58d5cabe17
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-11 18:57:39 -0400

    feat(core): import product provider service in providers

--- src/app/Core/core/core.module.ts
+++ src/app/Core/core/core.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductProviderService } from './providers/product-provider/product-provider.service';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    ProductProviderService
   ]
 })
 export class CoreModule { }
