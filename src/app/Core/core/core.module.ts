95dac468b5e0d76708a35f4d1d9d7ac5901d8be8
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-11 19:37:50 -0400

    feat(core): import http client module in core

--- src/app/Core/core/core.module.ts
+++ src/app/Core/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductProviderService } from './providers/product-provider/product-provider.service';
+import { HttpClientModule } from "@angular/common/http";
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   providers: [
     ProductProviderService
